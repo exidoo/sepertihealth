5d44851669473a8499a6b8cf63f50dbc0f0b9372
Romi RA <129487606+exidoo@users.noreply.github.com>
2023-10-12 20:12:19 +0700

    Update index.js
    Testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,37 +35,37 @@ const sr = ScrollReveal({
 
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
-sr.reveal(
-  `#header, .home_card, .service_content_detail,
-         .service_content_detail,
-         .choose_content, .choose_benefit,
-         .doctor_content, .swiper-wrapper,
-         .faq_detail, .faq_card, .appointment, 
-         .footer__container`,
-  {
-    origin: 'top',
-    interval: 100,
-  }
-);
+// sr.reveal(
+//   `#header, .home_card, .service_content_detail,
+//          .service_content_detail,
+//          .choose_content, .choose_benefit,
+//          .doctor_content, .swiper-wrapper,
+//          .faq_detail, .faq_card, .appointment, 
+//          .footer__container`,
+//   {
+//     origin: 'top',
+//     interval: 100,
+//   }
+// );
 
-sr.reveal(
-  `.home_detail, .about_img_hero
-         ,.service_detail,
-         .service_btn, .choose-card, .doctor_card,
-         .blog_button`,
-  {
-    origin: 'left',
-    interval: 100,
-  }
-);
+// sr.reveal(
+//   `.home_detail, .about_img_hero
+//          ,.service_detail,
+//          .service_btn, .choose-card, .doctor_card,
+//          .blog_button`,
+//   {
+//     origin: 'left',
+//     interval: 100,
+//   }
+// );
 
-sr.reveal(
-  `.home_pict, 
-         .about,
-         .card_service, .img-choose, .doctor_button, 
-         .testimonial_cs_title, .ourblog_content, .ourblog_card`,
-  {
-    origin: 'right',
-    interval: 100,
-  }
-);
+// sr.reveal(
+//   `.home_pict, 
+//          .about,
+//          .card_service, .img-choose, .doctor_button, 
+//          .testimonial_cs_title, .ourblog_content, .ourblog_card`,
+//   {
+//     origin: 'right',
+//     interval: 100,
+//   }
+// );
